670eb0e7b9fbd7939147eb7445456ddda0545242
Sivar-Developer <fragmatica.dc@gmail.com>
2022-01-14 22:15:14 +0300

    Get posts and upload image with image field

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,8 @@ class Post(models.Model):
     title = models.CharField(max_length=155)
     description = models.TextField()
     slug = models.SlugField(max_length=255)
-    image = models.ImageField(upload_to="image/", default="images/default.png")
+    image = models.ImageField(upload_to="images/", default="images/default.png")
     
     def __str__(self):
         return self.title
 
-    
