2b424660f740e9449acfee180a3d71c56e2f80d2
Sivar-Developer <fragmatica.dc@gmail.com>
2022-01-14 19:30:54 +0300

    Make migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=155)
+    description = models.TextField()
+    slug = models.SlugField(max_length=255)
+    
+    def __str__(self):
+        return self.title
+
+    
